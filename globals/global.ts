77b6b0bc9231f1d6c65dc5c7735f88381d3d7718
Moses Ngige <moses.soft.eng@gmail.com>
2023-08-03 15:01:54 +0300

    Create global.ts

---
+++ globals/global.ts
@@ -0,0 +1,12 @@
+/*
+ * Place all globally accessible varibles here.
+ */
+
+/*
+ * Global varibles for flow components.
+ */
+export const componentsTypes: Component[] = [
+  { id: -1, title: "Input Component", type: "input", nextComponentsIds: []},
+  { id: -1, title: "Command Component", type: "command", nextComponentsIds: []},
+  { id: -1, title: "Output Component", type: "output", nextComponentsIds: []},
+];
